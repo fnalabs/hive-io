106dbd21529bd093093424a311dfa01189ef381c
Adam Eilers <adam.eilers@gmail.com>
2017-04-15 00:15:49 -0700

    adding index

---
+++ packages/hive-js/index.js
@@ -0,0 +1,7 @@
+// expose library
+exports.Aggregate = require('./dist/js/Aggregate');
+exports.Command = require('./dist/js/Command');
+exports.Event = require('./dist/js/Event');
+exports.Handler = require('./dist/js/Handler');
+exports.Message = require('./dist/js/Message');
+exports.Schema = require('./dist/js/Schema');
