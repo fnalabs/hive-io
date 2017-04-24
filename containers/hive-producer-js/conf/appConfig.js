784afdb5e871b81ac76419418f92e6f05f50624e
Adam Eilers <adam.eilers@gmail.com>
2017-04-23 23:37:16 -0700

    adding initial app configuration for Producer

---
+++ containers/hive-producer-js/conf/appConfig.js
@@ -0,0 +1,12 @@
+module.exports = Object.freeze({
+    // application configurations
+    NODE_ENV: process.env.NODE_ENV || 'production',
+    PORT: process.env.PORT || 3000,
+    // domain configurations
+    MODEL: process.env.MODEL || 'view',
+    MODEL_LIB: process.env.AGGREGATE_LIB || './model',
+    // storage configurations
+    EVENT_STORE_ID: process.env.EVENT_STORE_ID,
+    EVENT_STORE_URL: process.env.EVENT_STORE_URL,
+    EVENT_STORE_TYPE: process.env.EVENT_STORE_TYPE || 2
+});
