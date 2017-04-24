64f61b83d5eb69c9f9cbd6e23420c8bc580189a9
Adam Eilers <adam.eilers@gmail.com>
2017-04-24 02:24:33 -0700

    tweaking comments and config property positions in the hash

--- containers/hive-stream-processor-js/conf/appConfig.js
+++ containers/hive-stream-processor-js/conf/appConfig.js
@@ -7,14 +7,15 @@ module.exports = Object.freeze({
     AGGREGATE: process.env.AGGREGATE || 'content',
     AGGREGATE_LIB: process.env.AGGREGATE_LIB || './domain',
     COMMAND_LIB: process.env.COMMAND_LIB || './commands',
-    // storage configurations
-    CACHE_URL: process.env.CACHE_URL,
+    // event store configurations
     EVENT_STORE_ID: process.env.EVENT_STORE_ID,
     EVENT_STORE_URL: process.env.EVENT_STORE_URL,
     EVENT_STORE_TYPE: process.env.EVENT_STORE_TYPE || 2,
     EVENT_STORE_TIMEOUT: process.env.EVENT_STORE_TIMEOUT || 15000,
     EVENT_STORE_PROTOCOL: process.env.EVENT_STORE_PROTOCOL || 'roundrobin',
     EVENT_STORE_OFFSET: process.env.EVENT_STORE_OFFSET || 'latest',
+    // storage configurations
+    CACHE_URL: process.env.CACHE_URL,
     LOCK_TTL: process.env.LOCK_TTL || 1000,
     LOCK_DRIFT_FACTOR: process.env.LOCK_DRIFT_FACTOR || 0.01,
     LOCK_RETRY_COUNT: process.env.LOCK_RETRY_COUNT || 0,
