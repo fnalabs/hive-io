c037207786528ee341676a8370c58272da9572fd
Adam Eilers <adam.eilers@gmail.com>
2018-02-11 15:48:03 -0800

    quick updates to README and index for refactor

--- packages/hive-js/index.js
+++ packages/hive-js/index.js
@@ -1,3 +1,9 @@
 // expose library
-exports.Handler = require('./dist/Handler')
+const actors = require('./dist/Actor')
+exports.Actor = actors.Actor
+exports.MessageActor = actors.MessageActor
+
 exports.Model = require('./dist/Model')
+exports.Schema = require('schema-json-js')
+
+exports.parse = require('./dist/util').parse
