b0b2dd9c554db390e8c2c74fdcaca56b2c1c90e4
Adam Eilers <adam.eilers@gmail.com>
2018-05-27 01:12:47 -0700

    adding example Actor System

---
+++ packages/hive-js-rest-example/src/actors/LogActor.js
@@ -0,0 +1,30 @@
+// imports
+import { parse, Actor, Schema } from 'hive-io'
+
+import fluentConnect from '../util/fluentConnect'
+
+import LogSchema from '../schemas/json/Log.json'
+
+/*
+ * class LogActor
+ */
+class LogActor extends Actor {
+  constructor (logSchema, logger) {
+    super(parse`/logs`, logSchema, logger)
+  }
+
+  async perform (model) {
+    this.repository.emit(LogSchema.title, { message: JSON.stringify(model) })
+  }
+}
+
+/*
+ * Proxy<LogActor>
+ */
+export default new Proxy(LogActor, {
+  construct: async function (LogActor) {
+    const logger = fluentConnect()
+    const logSchema = await new Schema(LogSchema)
+    return new LogActor(logSchema, logger)
+  }
+})
