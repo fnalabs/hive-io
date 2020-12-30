b5b37f3b6bbb07cb5506eeb6acc3aeef31757562
Adam Eilers <adam.eilers@gmail.com>
2020-12-30 00:15:04 -0600

    refactor(hive-io-rest-example): adding error handling and unit tests

--- packages/hive-js-rest-example/src/config.js
+++ packages/hive-js-rest-example/src/config.js
@@ -1,3 +1,8 @@
+import pkg from '../package.json'
+
+export const TELEMETRY_LIB_NAME = pkg.name
+export const TELEMETRY_LIB_VERSION = pkg.version
+
 export const MONGO_URL = process.env.MONGO_URL
 export const UPDATE_OPTIONS = Object.freeze({
   new: true
